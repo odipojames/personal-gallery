1c77cee86399dec0f2bbd0d828f092dd09789bd6
odipojames <odipojames12@gmai.com>
2018-12-07 10:17:28 +0300

    pics/url.py file

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pics.apps.NewsConfig',
+    'pics.apps.PicsConfig',
 ]
 
 MIDDLEWARE = [
