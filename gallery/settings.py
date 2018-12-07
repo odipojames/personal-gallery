74a26d28d8ebdc72c050d66a40c568a69635882b
odipojames <odipojames12@gmai.com>
2018-12-07 10:53:34 +0300

    ceate template/base.html file

--- gallery/settings.py
+++ gallery/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pics.apps.PicsConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
--- pics/views.py
+++ pics/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
 
 # Create your views here.
+def index(request):
+    try:
+        pass
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+    return render(request, 'index.html')
