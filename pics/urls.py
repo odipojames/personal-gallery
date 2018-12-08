7826c36f9021a042ef5888c96bdd1d903c813823
odipojames <odipojames12@gmai.com>
2018-12-08 11:18:46 +0300

    add copy link js function to index file

--- pics/urls.py
+++ pics/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.index,name='index'),
+    url(r'^search/', views.search, name='search'),
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- pics/views.py
+++ pics/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
+from .models import *
 
 # Create your views here.
 def index(request):
-    try:
-        pass
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-    return render(request, 'index.html')
+    locations = Places.objects.all()
+    images = Image.objects.all()
+    cat = Category.objects.all()
+    title = "Gallery"
+    if 'location' in request.GET and request.GET['location']:
+        town = request.GET.get('location')
+        pictures = Image.view_image(town)
+
+        return render(request, 'index.html', {'title': title, 'content': pictures})
+
+
+    elif 'category' in request.GET and request.GET['category']:
+        pictures = Image.image_cat(request.GET.get('category'))
+
+        return render(request, 'index.html', {'title': title, 'content': pictures})
+
+    return render(request, 'index.html',{'location': locations, 'content': images, 'category': cat})
+
+def search(request):
+
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        pictures = Image.search_image(search_term)
+
+        title = f'{search_term}'
+        return render(request,'search.html',{'title': title, 'content': pictures})
+
+    else:
+        message = "You haven't searched for any Category"
+        return render(request,'search.html')
