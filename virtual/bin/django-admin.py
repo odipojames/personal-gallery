318d74615092982ffd015bba59f8229c696059d6
odipojames <odipojames12@gmai.com>
2018-12-10 19:27:24 +0300

    push to heroku

--- virtual/bin/django-admin.py
+++ virtual/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/odipo/Documents/personal-gallery/virtual/bin/python3.6
+#!/home/odipo/Documents/personal-gallery/virtual/bin/python
 from django.core import management
 
 if __name__ == "__main__":
